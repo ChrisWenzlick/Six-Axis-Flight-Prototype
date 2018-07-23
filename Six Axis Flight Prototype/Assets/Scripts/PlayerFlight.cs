f4be1cbdfe69df7e50a85b635796a8bb6cd7f764
Chris Wenzlick <cwenzlick01@gmail.com>
2018-07-22 22:17:28 -0500

    Added Ship and Environment
    Created a simple player ship with pitch and roll controls. The ship automatically flies forward relative to its position. Also added a simple terrain for a reference point while flying, which the ship can't pass through.

---
+++ Six Axis Flight Prototype/Assets/Scripts/PlayerFlight.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFlight : MonoBehaviour {
+
+    public float playerSpeed = 90f;
+    public float playerYawSpeed = 5f;
+    public float playerPitchSpeed = 5f;
+    public float playerRollSpeed = 5f;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Player flight script added to " + gameObject.name);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += transform.forward * Time.deltaTime * playerSpeed;
+
+        transform.Rotate(Input.GetAxis("Vertical") * playerPitchSpeed, 0.0f, -Input.GetAxis("Horizontal") * playerRollSpeed);
+
+        float terrainHeightAtLocation = Terrain.activeTerrain.SampleHeight(transform.position);
+        if(terrainHeightAtLocation > transform.position.y)
+        {
+            transform.position = new Vector3(transform.position.x,
+                                             terrainHeightAtLocation,
+                                             transform.position.z);
+        }
+	}
+}
